7cdacbcb9ebd5e097f8a6374127cf546e5fea32a
Mumtaz Roy <mumtazroy@msn.com>
2016-11-15 01:38:33 +0000

    Twitter app

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,66 @@
+class TweetsController < ApplicationController
+    before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+    before_filter :authenticate_user!
+    
+    def create
+        @tweet = Tweet.new(tweet_params)
+        
+        respond_to do |format|
+            if @tweet.save
+                format.html { redirect_to @tweet, notice: 'Tweet was successfully created.'}
+            else
+                format.html { render :new }
+            end
+        end
+    end
+     
+    def update
+        @tweet = Tweet.new(tweet_params)
+        
+        respond_to do |format|
+            if @tweet.save
+                format.html { redirect_to @tweet, notice: 'Tweet was successfully created.'}
+            else
+                format.html { render :new }
+             end
+           end
+         end
+    def destroy
+        @tweet.destroy
+        respond_to do |format|
+            format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.'}
+        end
+    end
+        
+        
+        
+        
+        
+    def index
+        @tweets = Tweet.all
+    end
+    
+    def show
+    end
+
+    def new
+        @tweet = Tweet.new
+    end
+    
+    def edit
+    end
+    
+    private
+    
+    def set_tweet
+        @tweet = Tweet.find(params[:id])
+    end
+    
+    def tweet_params
+        params.require(:tweet).permit(:message, :user_id)
+    end
+        
+
+
+
+end
